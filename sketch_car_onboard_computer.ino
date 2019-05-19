28f38ab0f533b772cb658506ee39ed2bb855f07b
istvan <kosztik@gmail.com>
2019-05-19 19:14:37 +0200

    Add files via upload
    Manage fuel, using permanent data

--- sketch_car_onboard_computer.ino
+++ sketch_car_onboard_computer.ino
@@ -3,6 +3,16 @@
 
 #include <Wire.h>
 
+/*
+ * AT24C02 stored values:
+ *  0x50,0 : fuel
+ * 
+ * 
+ * 
+ */
+
+
+
 // Word to byte
 // byte highByte(int x)
 // byte lowByte(int x)
@@ -13,6 +23,9 @@ int NUM_KEYS = 5;
 int adc_key_in;
 int key=-1;
 int oldkey=-1;
+byte menu = 1;
+int fuel = 0;
+
 void setup()
 {
   
@@ -20,18 +33,21 @@ void setup()
   Wire.begin();
   Wire.setClock(400000);
   
+  fuel = (byte)i2c_eeprom_read_byte(0x50, 1);
+  //i2c_eeprom_write_byte(0x50, 0,0); // null my fuel register just once, when I start using AT24C02
+  
 }
+
 void loop()
 {
   adc_key_in = analogRead(0);    // read the value from the sensor
-  
   key = adc_key_in;  // convert into key press
-
   if (key != oldkey)   // if keypress is detected
    {
     delay(50);  // wait for debounce time
     adc_key_in = analogRead(0);    // read the value from the sensor
     key = adc_key_in;   
+    
     if (key != oldkey)
     {
       oldkey = key;
@@ -39,27 +55,58 @@ void loop()
         //Serial.println(key); // debug to show keycodes - it help to adjust your own AD keyboard button values
         switch(key)
         {
-
           //   ˇˇˇ - you will need to change these values for your own AD keyboard
-           case 0:Serial.println("sw 1");
+           case 0: // sw1
+                  Serial.println("sw 1");                                   
+                  // i2c_eeprom_read_byte(0x50, 1));
+
+                  // ha a 2-es menü alatt nyomjuk meg, a benzin -1L
+                  if (menu == 2) manageFuel(-1);
+                  break;
                   
-                  Serial.println(
-                    i2c_eeprom_read_byte(0x50, 1));
-                  break;
            case 20:
-           case 19:Serial.println("sw 2");
-                   i2c_eeprom_write_byte(0x50, 1,19);
+           case 19: // sw2
+                   Serial.println("sw 2");
+                   // i2c_eeprom_write_byte(0x50, 1,19);
+
+                   // ha a 2-es menü alatt nyomjuk meg, a benzin +5L
+                   if (menu == 2) manageFuel(+5);
+                   break;
+                                      
+           case 57: // sw3
+                  Serial.println("sw 3");
+
+                  // ha a 2-es menü alatt nyomjuk meg, a benzin -5L
+                  if (menu == 2) manageFuel(-5);
                   break;
-           case 57:Serial.println("sw 3");
-                  break;
-           case 109:Serial.println("sw 4");
+
+           case 110:
+           case 109: //sw4
+                  Serial.println("sw 4");
+                  
+                  // ha a 2-es menü alatt nyomjuk meg, a benzin +1L
+                  if (menu == 2) manageFuel(+1);
+                  
                   break;
            case 232:
-           case 233:Serial.println("sw 5");
+           case 233: // sw5 
+                  // Serial.println("sw 5");
+                  if (menu == 2 ) {menu = 1;}
+                  else {menu = 2;}
+                  
+                  Serial.println(menu);
                   break;
         }
       }
     }
+    /*
+     * Ide jön az 1-es menü kódja. Figyeli a odb2 szenzorokat és kalkulál.
+     * Akkor is, ha a 2-es menüben vagyok, vagyis nem kell 1 és 2 menü
+     * esetében if -es szerkezet. 
+     * 
+     */
+
+    
   }
  delay(100);
 }
@@ -87,4 +134,14 @@ void i2c_eeprom_write_byte(uint8_t deviceaddress, uint8_t eeaddress, uint8_t dat
   Wire.write((int)(eeaddress & 0xFF)); // LSB - Shift the integer right 8 bits
   Wire.write(rdata);
   Wire.endTransmission();
+}
+
+void manageFuel(int f) {
+
+  fuel += f;
+  if (fuel < 0) fuel = 0;
+  i2c_eeprom_write_byte(0x50, 1,(byte)fuel);
+  Serial.println(fuel);
+  
+    
 }
