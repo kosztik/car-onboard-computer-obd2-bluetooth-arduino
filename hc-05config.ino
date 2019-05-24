13726e82435714f37c31a481644ce4fa542d80d9
istvan <kosztik@gmail.com>
2019-05-24 06:42:18 +0200

    Create hc-05config.ino

---
+++ hc-05config.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+SoftwareSerial EEBlue(10, 11); // RX | TX
+ 
+void setup()
+{
+  Serial.begin(9600);
+  EEBlue.begin(38400);  //Baud Rate for command Mode. 
+  Serial.println("Enter AT commands!");
+}
+ 
+void loop()
+{
+ 
+  // Feed any data from bluetooth to Terminal.
+  if (EEBlue.available())
+    Serial.write(EEBlue.read());
+ 
+  // Feed all data from termial to bluetooth
+  if (Serial.available())
+    EEBlue.write(Serial.read());
+}
