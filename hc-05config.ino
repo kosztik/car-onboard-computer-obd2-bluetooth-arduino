28f0441e6f3715f9e2c3c74f3dda5b4146a31083
istvan <kosztik@gmail.com>
2019-07-10 11:57:21 +0200

    Simple prog to configura hc05 in AT mode
    and only those hc05 which have button to enter AT mode

--- hc-05config.ino
+++ hc-05config.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 SoftwareSerial EEBlue(10, 11); // RX | TX
  
-void setup() 
+void setup()  
 {
   Serial.begin(9600);
   EEBlue.begin(38400);  //Baud Rate for command Mode. 
